259dc8dea9f5965cf6e377a49e81bef2f50dce67
Adam <113860069+IslAdam@users.noreply.github.com>
2022-10-18 13:50:40 +0300

    Add files via upload

--- 1669d.cpp
+++ 1669d.cpp
@@ -1,16 +1,59 @@
 #include<iostream>
 
 int main() {
-    int n = 0;
-    std::cin >> n;
-    std::string s;
-    std::cin >> s;
-    for (int i = 0; i < n; i += 1) {
-        if (s[i] == 'W') {
-            for (int j = i; i < n; i += 1) {
+    int t = 0;
+    std::cin >> t; 
+    while(t--){
+        int n, allW = 0;
+        char c = ' ';
+        std::cin >> n;
+        std::string s;
+        std::cin >> s;
+        int f = 0;
+        for (int i = 0; i < n; i += 1) {
+            if (n < 2) {
+                std::cout << "No" << std::endl;
+                f = 1;
+                break;
+            }
+            if ((n == 2 && (s[0] != 'W' || s[1]!='W')) && (s[0] == 'W' || s[1] == 'W')) {
+                std::cout << "No" << std::endl;
+                f = 1;
+                break;
 
             }
+            for (int i = 0; i < n; i += 1) {
+                if (s[i] != 'W') {
+                    allW = 1;
+                }
+            }
+            if (allW != 1) {
+                std::cout << "Yes" << std::endl;
+                f = 1;
+                break;
+            }
+            if (s[i] == 'W') {
+                if (f == 1) {
+                    break;
+                }
+                for (int j = i+1; j < n; j += 1) {
+                    c = s[j];
+                    if (s[j + 1] != c && s[j + 1] != 'W') {
+                        break;
+                    }
+                    else if (s[j + 1] == 'W') {
+                        std::cout << "No" << std::endl;
+                        f = 1;
+                        break;
+                    }
+                }
+            }
+        
         }
-    };
+        if (f == 0) {
+            std::cout << "Yes" << std::endl;;
+        }
+    }
+    
     
 }
