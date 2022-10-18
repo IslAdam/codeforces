8770c03676263ba32e738de9f35f1973d44bd620
Adam <113860069+IslAdam@users.noreply.github.com>
2022-10-18 10:21:52 +0300

    Add files via upload

---
+++ 1669d.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+int main() {
+    int n = 0;
+    std::cin >> n;
+    std::string s;
+    std::cin >> s;
+    for (int i = 0; i < n; i += 1) {
+        if (s[i] == 'W') {
+            for (int j = i; i < n; i += 1) {
+
+            }
+        }
+    };
+    
+}
