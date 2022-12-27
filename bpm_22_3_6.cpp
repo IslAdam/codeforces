0a4d9850ec61282ee9bf08cd3c23fded352fb193
Adam <113860069+IslAdam@users.noreply.github.com>
2022-12-27 10:15:32 +0300

    Add files via upload

---
+++ bpm_22_3_6.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<vector>
+
+int main() {
+    int h = 0;
+    int w = 0;
+    int cw = 0;
+    
+    std::cin >> h;
+    std::cin >> w;
+    std::vector<std::vector<int> > a;
+    //std::cout << "0 - cws, 1 - cwc: ";
+    //std::cin >> cw;
+    
+}
